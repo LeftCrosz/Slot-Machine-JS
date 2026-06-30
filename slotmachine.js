f43f3860989a13b3e1cbb0fab1cfe309daa4c658
Wai Lam <leftcross112@gmail.com>
2026-06-30 19:44:25 +0800

    Implement user account creation and enhance game feedback

--- slotmachine.js
+++ slotmachine.js
@@ -3,6 +3,21 @@ const prompt = promptSync();
 
 import { User } from './user.js';
 
+// User account creation
+const createUser = () => {
+    while (true) {
+        let name = prompt("Enter your name: ");
+        if (name == "" || name.match(/[0-9]/)) {
+            console.log("Invalid name, try again!");
+        } else {
+            const nameUpper = name.charAt(0).toUpperCase() + name.slice(1);
+            const user1 = new User(nameUpper, 0);
+            console.log(`Created ${nameUpper} account successfully`)
+            return user1;
+        }
+    }
+}
+
 // Place bet
 const placeBet = () => {
     while (true) {
@@ -41,18 +56,31 @@ const rollslot = () => {
 const calcGame = () => {
     if (betNum == slotNum) {
         const winAmount = betAmount * 2;
-        balance *= 2;
-        console.log(`Congratulations! You won a total of RM${winAmount} from the Slot Machine :)`);
-
+        user1.balance *= 2;
+        console.log(`Rolled number is... ${slotNum}!`)
+        console.log(`Congratulations! ${user1.name} won a total of RM${winAmount} from the Slot Machine :)`);
         console.log(`Your bet amount: RM${betAmount}`);
-
     } else {
-        console.log(`Awww, you lost RM${betAmount}!`)
+        console.log(`Rolled number is... ${slotNum}!`)
+        console.log(`Awww ${user1.name}, you lost RM${betAmount}!`)
     }
 }
 
-// Intialize game
 
+// Slot Machine's Header
+const printHeader = () => { 
+    console.log(`
+    ░██████╗██╗░░░░░░█████╗░████████╗███╗░░░███╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗███████╗
+    ██╔════╝██║░░░░░██╔══██╗╚══██╔══╝████╗░████║██╔══██╗██╔══██╗██║░░██║██║████╗░██║██╔════╝
+    ╚█████╗░██║░░░░░██║░░██║░░░██║░░░██╔████╔██║███████║██║░░╚═╝███████║██║██╔██╗██║█████╗░░
+    ░╚═══██╗██║░░░░░██║░░██║░░░██║░░░██║╚██╔╝██║██╔══██║██║░░██╗██╔══██║██║██║╚████║██╔══╝░░
+    ██████╔╝███████╗╚█████╔╝░░░██║░░░██║░╚═╝░██║██║░░██║╚█████╔╝██║░░██║██║██║░╚███║███████╗
+    ╚═════╝░╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚══════╝
+`);
+}
+
+printHeader();
+let user1 = createUser();
 let betAmount = placeBet();
 let betNum = enterBetNum();
 let slotNum = rollslot();
